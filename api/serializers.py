710b08eb21dc654bc62df90ec7d9ee543c04d81e
okoye-collins <collins.kcj@gmail.com>
2022-07-23 14:13:29 +0100

    Done with the Api for the Todoapp

---
+++ api/serializers.py
@@ -0,0 +1,8 @@
+from operator import mod
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -2,15 +2,55 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from api.models import Task
+from .serializers import TaskSerializer
+from api import serializers
+
 # Create your views here.
 
-@api_view
+@api_view(['GET'])
 def apiOverview(request):
     api_urls = {
         'Task List': '/task-list/',
-        'Detail View': '/task-detail/',
-        'Update Task': '/task-update/',
+        'Detail View': '/task-detail/pk',
         'Create Task': '/task-create/',
-        'Delete Task': '/task-delete/',
+        'Update Task': '/task-update/pk',
+        'Delete Task': '/task-delete/pk',
     }
     return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return Response('Item was Deleted')
